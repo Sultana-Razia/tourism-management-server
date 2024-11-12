8dff92fc375e8305117329e7d3f6f60b34a1f111
Sultana Razia <sultanarazia483@gmail.com>
2024-11-12 18:00:05 +0600

    Delete myList data from database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -49,8 +49,15 @@ async function run() {
         })
 
         app.get('/myList/:email', async (req, res) => {
-            console.log(req.params.email);
+            // console.log(req.params.email);
             const result = await spotCollection.find({ email: req.params.email }).toArray();
+            res.send(result);
+        })
+
+        app.delete('myList/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await spotCollection.deleteOne(query);
             res.send(result);
         })
 
